791ce7d9c4b6edb10040f8f6c3221001209fd939
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2024-10-28 16:05:06 -0300

    LPD-40087 Implement tests for get and post workflowDefinitionLinks

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-test/src/testIntegration/java/com/liferay/headless/admin/workflow/resource/v1_0/test/WorkflowDefinitionLinkResourceTest.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-test/src/testIntegration/java/com/liferay/headless/admin/workflow/resource/v1_0/test/WorkflowDefinitionLinkResourceTest.java
@@ -8,8 +8,6 @@ package com.liferay.headless.admin.workflow.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowDefinition;
 import com.liferay.headless.admin.workflow.client.dto.v1_0.WorkflowDefinitionLink;
-import com.liferay.headless.admin.workflow.client.pagination.Page;
-import com.liferay.headless.admin.workflow.client.pagination.Pagination;
 import com.liferay.headless.admin.workflow.resource.v1_0.test.util.WorkflowDefinitionTestUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
@@ -17,18 +15,16 @@ import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
 
-import java.util.List;
+import java.util.Collections;
+import java.util.Map;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -39,29 +35,20 @@ import org.junit.runner.RunWith;
 public class WorkflowDefinitionLinkResourceTest
 	extends BaseWorkflowDefinitionLinkResourceTestCase {
 
-	public WorkflowDefinitionLink addWorkflowDefinitionLink(
-			Long workflowDefinitionId,
-			WorkflowDefinitionLink workflowDefinitionLink)
-		throws Exception {
-
-		return workflowDefinitionLinkResource.postWorkflowDefinitionLink(
-			workflowDefinitionId, workflowDefinitionLink);
-	}
-
 	@Before
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
 
-		_workflowDefinition =
-			WorkflowDefinitionTestUtil.addWorkflowDefinition();
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(
+				UserLocalServiceUtil.getUser(TestPropsValues.getUserId())));
 
 		_objectDefinition =
 			ObjectDefinitionTestUtil.addCustomObjectDefinition();
 
-		PermissionThreadLocal.setPermissionChecker(
-			PermissionCheckerFactoryUtil.create(
-				UserLocalServiceUtil.getUser(TestPropsValues.getUserId())));
+		_workflowDefinition =
+			WorkflowDefinitionTestUtil.addWorkflowDefinition();
 	}
 
 	@After
@@ -81,66 +68,6 @@ public class WorkflowDefinitionLinkResourceTest
 	}
 
 	@Override
-	@Test
-	public void testGetWorkflowDefinitionLinks() throws Exception {
-		Long workflowDefinitionId = _workflowDefinition.getId();
-
-		Page<WorkflowDefinitionLink> page =
-			workflowDefinitionLinkResource.getWorkflowDefinitionLinks(
-				workflowDefinitionId, Pagination.of(1, 10));
-
-		long totalCount = page.getTotalCount();
-
-		WorkflowDefinitionLink workflowDefinitionLink1 =
-			addWorkflowDefinitionLink(
-				workflowDefinitionId, randomWorkflowDefinitionLink());
-
-		WorkflowDefinitionLink workflowDefinitionLink2 =
-			addWorkflowDefinitionLink(
-				workflowDefinitionId, randomWorkflowDefinitionLink());
-
-		page = workflowDefinitionLinkResource.getWorkflowDefinitionLinks(
-			workflowDefinitionId, Pagination.of(1, 10));
-
-		Assert.assertEquals(totalCount + 2, page.getTotalCount());
-
-		assertContains(
-			workflowDefinitionLink1,
-			(List<WorkflowDefinitionLink>)page.getItems());
-		assertContains(
-			workflowDefinitionLink2,
-			(List<WorkflowDefinitionLink>)page.getItems());
-		assertValid(
-			page,
-			testGetWorkflowDefinitionLinks_getExpectedActions(
-				workflowDefinitionId));
-
-		WorkflowDefinitionLinkLocalServiceUtil.deleteWorkflowDefinitionLink(
-			workflowDefinitionLink1.getId());
-		WorkflowDefinitionLinkLocalServiceUtil.deleteWorkflowDefinitionLink(
-			workflowDefinitionLink2.getId());
-	}
-
-	@Override
-	@Test
-	public void testPostWorkflowDefinitionLink() throws Exception {
-		WorkflowDefinitionLink randomWorkflowDefinitionLink =
-			randomWorkflowDefinitionLink();
-
-		WorkflowDefinitionLink postWorkflowDefinitionLink =
-			addWorkflowDefinitionLink(
-				_workflowDefinition.getId(), randomWorkflowDefinitionLink);
-
-		randomWorkflowDefinitionLink.setWorkflowDefinitionName(
-			_workflowDefinition.getName());
-		randomWorkflowDefinitionLink.setWorkflowDefinitionVersion(
-			Integer.valueOf(_workflowDefinition.getVersion()));
-
-		assertEquals(randomWorkflowDefinitionLink, postWorkflowDefinitionLink);
-		assertValid(postWorkflowDefinitionLink);
-	}
-
-	@Override
 	protected WorkflowDefinitionLink randomWorkflowDefinitionLink()
 		throws Exception {
 
@@ -150,6 +77,54 @@ public class WorkflowDefinitionLinkResourceTest
 				groupId = testGroup.getGroupId();
 			}
 		};
+	}
+
+	@Override
+	protected WorkflowDefinitionLink
+			testGetWorkflowDefinitionByExternalReferenceCodeWorkflowDefinitionLinksPage_addWorkflowDefinitionLink(
+				String externalReferenceCode,
+				WorkflowDefinitionLink workflowDefinitionLink)
+		throws Exception {
+
+		return workflowDefinitionLinkResource.
+			postWorkflowDefinitionByExternalReferenceCodeWorkflowDefinitionLink(
+				externalReferenceCode, workflowDefinitionLink);
+	}
+
+	@Override
+	protected String
+			testGetWorkflowDefinitionByExternalReferenceCodeWorkflowDefinitionLinksPage_getExternalReferenceCode()
+		throws Exception {
+
+		return _workflowDefinition.getExternalReferenceCode();
+	}
+
+	@Override
+	protected Map<String, Map<String, String>>
+			testGetWorkflowDefinitionWorkflowDefinitionLinksPage_getExpectedActions(
+				Long workflowDefinitionId)
+		throws Exception {
+
+		return Collections.emptyMap();
+	}
+
+	@Override
+	protected Long
+			testGetWorkflowDefinitionWorkflowDefinitionLinksPage_getWorkflowDefinitionId()
+		throws Exception {
+
+		return _workflowDefinition.getId();
+	}
+
+	@Override
+	protected WorkflowDefinitionLink
+			testPostWorkflowDefinitionByExternalReferenceCodeWorkflowDefinitionLink_addWorkflowDefinitionLink(
+				WorkflowDefinitionLink workflowDefinitionLink)
+		throws Exception {
+
+		return workflowDefinitionLinkResource.
+			postWorkflowDefinitionWorkflowDefinitionLink(
+				_workflowDefinition.getId(), workflowDefinitionLink);
 	}
 
 	private static ObjectDefinition _objectDefinition;
