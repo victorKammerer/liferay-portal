4a909fb62aba955c62a008264f8655a2e3e0c89c
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-02-18 17:20:13 -0300

    LPD-49224 gw buildUpgradeTable

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v10_5_0/util/ObjectEntryVersionTable.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v10_5_0/util/ObjectEntryVersionTable.java
@@ -5,8 +5,31 @@
 
 package com.liferay.object.internal.upgrade.v10_5_0.util;
 
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
 /**
  * @author Feliphe Marinho
+ * @generated
+ * @see com.liferay.portal.tools.upgrade.table.builder.UpgradeTableBuilder
  */
 public class ObjectEntryVersionTable {
+
+	public static UpgradeProcess create() {
+		return new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				if (!hasTable(_TABLE_NAME)) {
+					runSQL(_TABLE_SQL_CREATE);
+				}
+			}
+
+		};
+	}
+
+	private static final String _TABLE_NAME = "ObjectEntryVersion";
+
+	private static final String _TABLE_SQL_CREATE =
+		"create table ObjectEntryVersion (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,objectEntryVersionId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,objectEntryId LONG,content TEXT null,version INTEGER,status INTEGER)";
+
 }
